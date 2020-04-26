16a9c32e5660a318c90f373531d5cb04e1c293d0
reazur <reazur@ares2.bio.brandeis.edu>
2020-04-26 11:17:02 -0400

    update the process for applying read and edit threshold

--- CODE/rnaedit_gDNA_RNA.sh
+++ CODE/rnaedit_gDNA_RNA.sh
@@ -6,11 +6,6 @@ HyperTRIBE_DIR="/home/analysis/editing/HyperTRIBE/CODE"
 #---------------------------
 # edit the following varibales as need
 annotationfile="/home/analysis/genome/dm6/refFlat.txt"
-#gDNAtablename="s2_gDNA"
-#gDNAexp="s2_gDNA"
-#gDNAtp="25"
-#RNAtablename="testRNA"
-#RNAexp="rnalibs"
 
 gDNAtablename="protocol_testDB"
 gDNAexp="gDNA"
@@ -28,15 +23,33 @@ for tp in ${timepoint[@]}
 do
   outfile1=$RNAexp"_"$gDNAtp"_"$tp"_A2G.txt"
   perl $HyperTRIBE_DIR/find_rnaeditsites.pl -a $annotationfile -t $RNAtablename -e $RNAexp -c $tp -o $outfile1 -g $gDNAtablename -j $gDNAexp -k $gDNAtp  
-# filter editsites based on cut off 20 reads, it is hard coded in the python script
-# to change the threshold, chnage the value in the python script TotalCountThreshold = 10
-#
-  python $HyperTRIBE_DIR/Threshold_editsites_20reads.py $outfile1
-  outfile2=$outfile1".threshold"
 
-  python $HyperTRIBE_DIR/convert_editsites_to_bedgraph.py $outfile2 
-  prefix=${outfile2%.txt.threshold*}
-  mv $outfile2".bedgraph" $prefix".bedgraph"
+  # Previously used Threshold_editsites_20reads.py script is replaced with filter_by_threshold_without_header.pl 
+
+  # convert to bedgraph format
+  prefix=${outfile1%.txt*}
+  python $HyperTRIBE_DIR/convert_editsites_to_bedgraph.py $outfile1
+  mv $outfile1".bedgraph" $prefix".bedgraph"
+
+  # apply edit % and read threshold, the index is zero based in the perl script
+  # 4th col is edit thresold: $threshold
+  # 21st column has a read threshold of 10
+  # create a 5% threshold edit file
+  edit_threshold=5
+  read_threshold=20
+  prefix=${outfile1%.txt*}
+  out_bedgraph=$prefix"_"$edit_threshold"%.bedgraph"
+  perl $HyperTRIBE_DIR/filter_by_threshold_without_header.pl 3 $edit_threshold 20 $read_threshold $prefix".bedgraph" > $out_bedgraph
+
+
+# the following code has been replace with the code above. Empirical data from our lab has shown that keeping the threshold at 5% provides a reliable target list. But, users can change it as needed.
+
+#  python $HyperTRIBE_DIR/Threshold_editsites_20reads.py $outfile1
+#  outfile2=$outfile1".threshold"
+#
+#  python $HyperTRIBE_DIR/convert_editsites_to_bedgraph.py $outfile2 
+#  prefix=${outfile2%.txt.threshold*}
+#  mv $outfile2".bedgraph" $prefix".bedgraph"
 #  echo $outfile2 $prefix".bedgraph"
 
 # ----------------------------
